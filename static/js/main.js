bb67f0041cf81723c4db58d0b129c2070dc696a4
Artem <84192047+frizyyu@users.noreply.github.com>
2025-05-26 03:22:31 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ document.getElementById('aboutMeButton').addEventListener('click', function(even
     document.querySelector('.scene').style.transform = 'translateX(-100%)';
     document.querySelector('.second-scene').style.transform = 'translateX(0)';
     document.getElementById('secondSceneHeading').textContent = 'About me:';
-document.getElementById('secondSceneText').innerHTML = '<p>Hi, my name is Artem</p><p>At the moment I am studying at ITMO University under the system application software program 09.03.04.</p><p>I started programming from the very beginning of 8th grade. During this time, I managed to pass 2 years of Yandex Lyceum with honors (basic + additional), participate in a hackathon and study the following programming languages to varying degrees:</p><ul><li>java</li><li>postgresql</li><li>HTML</li><li>CSS</li><li>JavaScript</li><li>python</li></ul>';
+document.getElementById('secondSceneText').innerHTML = '<p>Hi, my name is Artem</p><p>At the moment I am studying at ITMO University under the system application software program 09.03.04.</p><p>I started programming from the very beginning of 8th grade. During this time, I managed to pass 2 years of Yandex Lyceum with honors (basic + additional), participate in a hackathon, developed commercial projects, doing an internship at T-bank and study the following programming languages to varying degrees:</p><ul><li>JavaScript</li><li>TypeScript</li><li>java</li><li>postgresql</li><li>python</li></ul>';
     document.getElementById('aboutMePhoto').style.display = 'block';
     document.getElementById('table-projects').style.display = 'none';
     document.getElementById('links').style.display = 'none';
